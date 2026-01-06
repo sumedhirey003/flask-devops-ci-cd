d2b7327acf206d2d94d862cbd713881a756d9169
sumedhirey003 <sumedhhirey003@gmail.com>
2026-01-06 23:04:51 +0530

    Install pip using ensurepip and use system Python

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,14 +4,17 @@ set -e
 echo "Install Dependencies.sh...."
 
 APP_DIR="/home/ec2-user/flask-app"
-
-
 mkdir -p "$APP_DIR"
 cd "$APP_DIR"
 
 echo "Python version:"
 python3 --version
 
+echo "Ensuring pip is installed..."
+python3 -m ensurepip --upgrade
+
+echo "Upgrading pip..."
 python3 -m pip install --user --upgrade pip
 
+echo "Installing application dependencies..."
 python3 -m pip install --user -r requirements.txt
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Starting Flask app..."
+echo "Install Dependencies.sh...."
 
 APP_DIR="/home/ec2-user/flask-app"
+mkdir -p "$APP_DIR"
 cd "$APP_DIR"
 
-export PATH=$PATH:/home/ec2-user/.local/bin
+echo "Python version:"
+python3 --version
 
-nohup python3 app/main.py > app.log 2>&1 &
+echo "Ensuring pip is installed..."
+python3 -m ensurepip --upgrade
+
+echo "Upgrading pip..."
+python3 -m pip install --user --upgrade pip
+
+echo "Installing application dependencies..."
+python3 -m pip install --user -r requirements.txt
