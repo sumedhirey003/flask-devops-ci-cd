8b2d7be031f17531d7bdaf548caf3d280f56f0e5
sumedhirey003 <sumedhhirey003@gmail.com>
2026-01-06 22:48:45 +0530

    Install python3-venv before creating virtualenv

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,21 +5,23 @@ echo "Install Dependencies.sh...."
 
 APP_DIR="/home/ec2-user/flask-app"
 
-# Ensure app directory exists
+
 mkdir -p "$APP_DIR"
 
 cd "$APP_DIR"
 
-# Ensure Python is available
+echo "Python version:"
 python3 --version
 
-# Create virtual environment if missing
-if [ ! -d "$APP_DIR/venv" ]; then
+echo "Installing python3-venv if missing..."
+sudo yum install -y python3-venv
+
+if [ ! -d "$APP_DIR/venv/bin" ]; then
   echo "Creating virtual environment..."
   python3 -m venv "$APP_DIR/venv"
 fi
 
-# Activate venv
+
 source "$APP_DIR/venv/bin/activate"
 
 pip install --upgrade pip
